cf31a6f933658d9e7314af745153d79bcb7426ba
Светка <sveta.chakrygina@mail.ru>
2022-08-19 22:04:39 +0300

    Задание №1

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,37 @@ public class Main {
         int milkWeight = 105 * 2;
         int icecreamWeight = 100 * 2;
         int eggsWeight = 70 * 4;
+        System.out.println((bananasWeight + milkWeight + icecreamWeight + eggsWeight) / 1000);
+
+        // Задание №4
+        int weightOfSportsman = 7 * 1000;
+        int weight1 = 250;
+        int weight2 = 500;
+        System.out.println(weightOfSportsman / weight1 + " дней");
+        System.out.println(weightOfSportsman / weight2 + " дней");
+
+        //Задание №5
+        int Masha = 67760;
+        int Denis = 83690;
+        int Christina = 76230;
+        int increaseMasha = 67760 * 10 / 1000;
+        int increaseDenis = 83690 * 10 / 1000;
+        int increaseChristina = 76230 * 10 / 1000;
+        int increaseSumMasha = Masha + increaseMasha;
+        int increaseSumDenis = Denis + increaseDenis;
+        int increaseSumChristina = Christina + increaseChristina;
+        int MashaIncome = Masha * 12;
+        int DenisIncome = Denis * 12;
+        int ChristinaIncome = Christina * 12;
+        int MashaIncomeInc = (Masha + increaseMasha) * 12;
+        int DenisIncomeInc = (Denis + increaseDenis) * 12;
+        int ChristinaIncomeInc = (Christina + increaseChristina) * 12;
+        int incomeDifferenceMasha = MashaIncomeInc - MashaIncome;
+        int incomeDifferenceDenis = DenisIncomeInc - DenisIncome;
+        int incomeDifferenceChristina = ChristinaIncomeInc - ChristinaIncome;
+        System.out.println("Маша теперь получает " + MashaIncomeInc + "." + " Годовой доход вырос на " + incomeDifferenceMasha);
+        System.out.println("Денис теперь получает " + DenisIncomeInc + "." + " Годовой доход вырос на " + incomeDifferenceDenis);
+        System.out.println("Кристина теперь получает " + ChristinaIncomeInc + "." + " Годовой доход вырос на " + incomeDifferenceChristina);
     }
 
 
