11a9c882fecd0e3cf9e010239c99617906c4e955
Светка <sveta.chakrygina@mail.ru>
2022-08-19 21:28:27 +0300

    Задание №1

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,33 @@ public class Main {
         double aPieceOfCake = 6.7;
         char bar = 35;
         boolean d = 23 > 12;
-        System.out.println (cat);
-        System.out.println (penguin);
-        System.out.println (bear);
-        System.out.println (dolphin);
-        System.out.println (chocolateWeight);
-        System.out.println (aPieceOfCake);
-        System.out.println (bar);
-        System.out.println (d);
+        System.out.println(cat);
+        System.out.println(penguin);
+        System.out.println(bear);
+        System.out.println(dolphin);
+        System.out.println(chocolateWeight);
+        System.out.println(aPieceOfCake);
+        System.out.println(bar);
+        System.out.println(d);
+
+
+        // Задание №2
+        double Boxer1 = 78.2;
+        double Boxer2 = 82.7;
+        double allBoxers = Boxer1 + Boxer2;
+        double BoxersDifference = Boxer2 - Boxer1;
+        System.out.println(allBoxers);
+        System.out.println(BoxersDifference);
+
+        // Задание №3
+        int bananas = 5;
+        int milk = 200;
+        int icecream = 2;
+        int eggs = 4;
+        int bananasWeight = 80 * 5;
+        int milkWeight = 105 * 2;
+        int icecreamWeight = 100 * 2;
+        int eggsWeight = 70 * 4;
     }
 
 
